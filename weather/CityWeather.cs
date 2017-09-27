2477c415f47dc8f2437794ae9713329d577c4e28
gofaadil <gofaadil@live.com>
2017-09-28 00:47:30 +0530

    Initial commit

---
+++ weather/CityWeather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weather
+{
+    class CityWeather
+    {
+        public int Id { get; set; }
+        public double temperature { get; set; }
+        public double humidity { get; set; }
+        public string cloud_condition { get; set; }
+        public double wind_speed { get; set; }
+    }
+}
--- weather/MainWindow.xaml.cs
+++ weather/MainWindow.xaml.cs
@@ -12,17 +12,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace weather
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        AllWeather _allWeather;
+        const string byCityName = @"http://api.openweathermap.org/data/2.5/weather?q={0},Lk&appid=6b10c51dd696fece6817ad9d8e9b6936";
+        string response;
         public MainWindow()
         {
             InitializeComponent();
+            _allWeather = new AllWeather();
+        }
+        static string city;
+       
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+                    
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            city = cityComboBox.Text;
+            string ex = string.Format(byCityName, city);
+            HttpClient httpClinet = new HttpClient();
+            try
+            {
+                response = await httpClinet.GetStringAsync(ex);
+            }
+            catch (Exception httpex)
+            {
+                MessageBox.Show(httpex.Message + " Check your Internet Connection and Try Again!");
+            }
+            _allWeather = JsonConvert.DeserializeObject<AllWeather>(response);
+
+            resultTextBox.Text = Convert.ToString(Math.Round( Convert.ToDouble(_allWeather.main.temp), 2)-273.15);
+
+
+
+            //resultTextBox.Text = response;
         }
     }
 }
+
